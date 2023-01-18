ede76b0278b1281823839ea5995c19748327b464
yashwanthreddy-909 <yaswanthreddy909@gmail.com>
2023-01-18 18:20:52 +0530

    fizzBuzz done

--- main.js
+++ main.js
@@ -63,9 +63,8 @@ function capitalizeWords(str) {
 function maxCharacter(str) {
   let charCount = {}
   for(let i=0;i<str.length;i++) {
-    if(str[i] in charCount){
+    if(str[i] in charCount)
       charCount[str[i]]++
-    }
     else
       charCount[str[i]] = 1
   }
@@ -80,7 +79,19 @@ function maxCharacter(str) {
 
 
 // 6. Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  for(let i=1;i<100;i++) {
+    if(i%3===0 && i%5===0){
+      console.log("FizzBuzz")
+    }
+    else if(i%3==0)
+      console.log("Fizz")
+    else if(i%5==0)
+      console.log("Buzz")
+    else
+      console.log(i)
+  }
+}
 
 
 
@@ -166,6 +177,6 @@ function missingLetters() {}
  */
 function evenOddSums() {}
 
-console.log(maxCharacter('javascript'))
+fizzBuzz()
 
 
