c7a26b61b89f1186d482f35e1b0285d072b3e8d6
Tarak <tarakaswathi2001@gmail.com>
2023-01-19 18:27:37 +0530

    <fizzBuzz> done

--- main.js
+++ main.js
@@ -70,7 +70,26 @@ function maxCharacter(str) {
 
 }
 // 6. Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  var arr=[];
+  for(var i=1;i<=100;i++){
+    if (i%3==0 && i%5==0){
+      arr[i]="FizzBuzz";
+    }
+    else if (i%3==0){
+      arr[i]="Fizz";
+    }
+    else if (i%5==0){
+      arr[i]="Buzz";
+    }
+    else{
+      arr[i]=i;
+    }
+  }
+  return arr;
+}
+
+
 
 
 
