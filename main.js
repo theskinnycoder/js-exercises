4ea2d0d553aef232a19fa7def7fb1b7dd594b750
Prasanna Kumar <prasannakumar@pop-os.localdomain>
2023-01-24 19:20:38 +0530

    sortByHeight

--- main.js
+++ main.js
@@ -157,7 +157,21 @@ function seekAndDestroy(arr, ...values) {
 /* 15. Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
  * ex. sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]) == [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight() {}
+function sortByHeight(arr) {
+  let indices = []
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] == -1) {
+      indices.push(i);
+    }
+  }
+   
+  arr = arr.filter((item)=>item!==-1).sort((a, b) => a - b)
+  for (let i=0; i<indices.length; i++) {
+    arr.splice(indices[i], 0, -1);
+  }
+
+  return arr;
+}
 
 
 
