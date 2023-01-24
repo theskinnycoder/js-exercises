1c477fa1eb76d7ac54a9b1f47bcfe458e6e564c4
Prasanna Kumar <prasannakumar@pop-os.localdomain>
2023-01-24 19:18:44 +0530

    addAll

--- main.js
+++ main.js
@@ -114,7 +114,9 @@ function letterChanges(str) {
 /* 12. Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
  * ex. addAll(2,5,6,7) === 20
  */
-function addAll() {}
+function addAll(...args) {
+  return args.reduce((a, b)=>a + b, 0);
+}
 
 
 
