2c5f5f6c8dc6f205f7b0d8ca28f760fe5a6f4871
Surya Teja <tejasurya2002@gmail.com>
2023-04-18 14:50:18 +0530

    flattenArray done

--- main.js
+++ main.js
@@ -145,7 +145,17 @@ function chunkArray(arr, len) {
 /* 9. Take an array of arrays and flatten to a single array
  * ex. flattenArray([[1, 2], [3, 4], [5, 6], [7]]) should return [1, 2, 3, 4, 5, 6, 7]
  */
-function flattenArray(arrays) {}
+function flattenArray(arrays) {
+    let s = []
+    for (const i of arrays) {
+        if (Array.isArray(i)) {
+            s = s.concat(i)
+        } else {
+            s.push(i)
+        }
+    }
+    return s
+}
 
 
 
